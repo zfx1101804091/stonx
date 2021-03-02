41df727348bbc4d28f72a16aa3eb33b1571d03cc
zfx1101804091 <1101804091@qq.com>
2021-03-02 22:36:56 +0800

    Initial commit

---
+++ stonex-auth/src/main/java/org/stonx/auth/StoneXAuthApplication.java
@@ -0,0 +1,13 @@
+package org.stonx.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+@EnableHystrix
+@EnableFeignClients(basePackages = {"com.itheima.security.distributed.uaa"})
+public class StoneXAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StoneXAuthApplication.class,args);
+    }
+}
